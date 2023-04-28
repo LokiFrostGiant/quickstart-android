271fb0838c613d93dd0ff7c70c84b0b256121dd1
LokiFrostGiant <colinjohnson@mts.net>
2023-04-28 16:43:27 -0500

    basic compose setup

---
+++ storage/app/src/main/java/com/google/firebase/quickstart/firebasestorage/kotlin/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.google.firebase.quickstart.firebasestorage.kotlin.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+// self-defined light-mode colour scheme
+val FirebaseBlue = Color(0xFF0288D1)        // copied from colors.xml
+val FirebaseBannerBlue = Color(0xFF039BE5)  // copied from colors.xml
+val FirebaseOrange = Color(0xFFFFA000)      // copied from colors.xml
